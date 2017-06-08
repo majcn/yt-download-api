9b577c07aa626233669304edb05340b9eccdbc43
Gregor Majcen <gregor.majcen@sinergise.com>
2017-06-08 10:25:05 +0200

    short

--- webapp/index.js
+++ webapp/index.js
@@ -6,7 +6,7 @@ const app = new Koa();
 
 
 const PORT = process.env.PORT || 3000;
-const youtubedl = `${__dirname}/youtube-dl`;
+const youtubedl = 'youtube-dl';
 
 
 function update() {
@@ -16,22 +16,18 @@ function update() {
 
 function download(vid) {
     let buffer = execFileSync(youtubedl, ['-k', '-f', 'bestaudio', '-x', '--audio-quality', '0', '--audio-format', 'mp3', `http://www.youtube.com/watch?v=${vid}`])
-    return buffer.toString('utf8').split('\n').filter(e => e).slice(-1)[0]
+    return buffer.toString('utf8').split('\n').filter(e => e).slice(-1)[0].match(/^\[ffmpeg\] Destination: (.*\.mp3)$/)[1]
 }
 
 app.use(ctx => {
   let vid = (ctx.request.url.match(/^\/([a-zA-Z0-9_-]{11})$/) || [null, null])[1];
   if (vid) {
     let updateLog = update()
-    let downloadLog = download(vid)
-
-    let fileName = downloadLog.match(/^\[ffmpeg\] Destination: (.*\.mp3)$/)[1]
-    let fullFileName = `${__dirname}/${fileName}`;
-    let fileSize = fs.statSync(fullFileName).size;
+    let fileName = download(vid)
 
     ctx.res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
-    ctx.res.setHeader('Content-Length', fileSize);
-    ctx.body = fs.createReadStream(fullFileName);
+    ctx.res.setHeader('Content-Length', fs.statSync(fileName).size);
+    ctx.body = fs.createReadStream(fileName);
 
     console.log(`${vid}; ${updateLog}; ${fileName}`)
   }
